ac4ffb32ab18ab717713bb61c4263d611e22478e
Bishwash3 <bhusalbiswash2.com>
2024-10-29 09:59:27 +0545

    add authentication and tokens in user model

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -55,4 +55,42 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()    
+})
+
+userSchema.method.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.method.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("user", userSchema)
