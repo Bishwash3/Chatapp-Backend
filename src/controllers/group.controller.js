0d49518beb44f1d4a6f715b0dd8849f1a987e920
Bishwash3 <bhusalbiswash2.com>
2024-12-01 13:31:47 +0545

    add group controller

---
+++ src/controllers/group.controller.js
@@ -0,0 +1,37 @@
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResonse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../model/user.model.js"
+
+const createGroup = asyncHandler(async(req, res) => {
+
+})
+
+const updateGroup = asyncHandler(async(req, res) => {
+    
+})
+
+const deleteGroup = asyncHandler(async(req, res) => {
+    
+})
+
+const addMember = asyncHandler(async(req, res) => {
+    
+})
+
+const removeMember = asyncHandler(async(req, res) => {
+    
+})
+
+const transferAdmin = asyncHandler(async(req, res) => {
+    
+})
+
+export {
+    createGroup,
+    updateGroup,
+    deleteGroup,
+    addMember,
+    removeMember,
+    transferAdmin
+}
