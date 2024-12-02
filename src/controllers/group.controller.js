12324ba67541b5a30933050b20f8b03ae0a36729
Bishwash3 <bhusalbiswash2.com>
2024-12-02 14:15:15 +0545

    add update group endpoint

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -36,7 +36,55 @@ const createGroup = asyncHandler(async(req, res) => {
 })
 
 const updateGroup = asyncHandler(async(req, res) => {
-    
+    const {groupName, description} = req.body
+    const {groupId} = req.params
+
+    if(!groupId){
+        throw new ApiError(400, "Required Group Id")
+    }
+
+    const updatedGroupName = groupName?.trim()
+    const updatedDescription = description?.trim()
+
+    if(!updatedGroupName && !updatedDescription){
+        throw new ApiError(400, "Fields are empty to update")
+    }
+
+    const group = await Group.findById({groupId})
+
+    if(!group){
+        throw new ApiError(400, "Group Not Found")
+    }
+
+    if(req.user?._id.toString() !== group.adminId.toString()){
+        throw new ApiError(400, "You are not authorized to update group details")
+    }
+
+    const updatedFields = {};
+    if (updatedGroupName) updatedFields.groupName = updatedGroupName;
+    if (updatedDescription) updatedFields.description = updatedDescription;
+
+    const updatedGroupdetails = await Group.findByIdAndUpdate(
+        group._id,
+        updatedFields,
+        {
+            new: true
+        }
+        )
+
+        if(!updatedGroupdetails){
+            throw new ApiError(400, "Failed to update group detils")
+        }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updatedGroupdetails,
+            "Group updated succesfully"
+        )
+    )
 })
 
 const deleteGroup = asyncHandler(async(req, res) => {
